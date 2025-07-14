3a8f14cc43e5fa2cc395fc8f4aa7570c5465b841
insertNamehere123456 <ottoknight0603@gmail.com>
2025-07-14 14:57:51 -0400

    Main menu

--- Hotel Hex/Assets/Scripts/GameManager.cs
+++ Hotel Hex/Assets/Scripts/GameManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (instance == null)
+        {
+
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    public string Mainroom = "Main room";
+    public string mainmenu = "Main menu";
+    // Update is called once per frame
+    public void LoadScene(string name)
+    {
+        SceneManager.LoadScene(name);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
